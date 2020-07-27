cef22b2cb4b6b2163dd717b81e7501873c58a5da
Shubham-hub278 <shubhnitkkr26@gmail.com>
2020-07-27 20:25:06 +0530

    PartA file

---
+++ PartA.java
@@ -0,0 +1,60 @@
+package com.company;
+import java.util.Stack;
+import java.util.Scanner;
+
+    public class PartA {
+
+        private static Stack<String> history = new Stack<String>();
+
+        public static Boolean isBrowsingHistoryEmpty() {
+
+            return history.isEmpty();
+
+        }
+
+        public static String mostRecentlyVisitedSite() {
+
+            return history.peek();
+        }
+
+        public static void addSiteToHistory(String url) {
+
+            history.push(url);
+        }
+
+        public static void goBackInTime(int n) {
+            while(n>0&& !history.isEmpty()){
+                history.pop();
+                n--;
+            }
+        }
+
+        public static void printBrowsingHistory() {
+            try {
+                if (!history.isEmpty()) {
+                    System.out.println(history);
+                }
+            }
+            catch(Exception e){
+                System.out.println("Browsing history is empty");
+            }
+
+        }
+
+        public static void main(String[] args) {
+            System.out.println(isBrowsingHistoryEmpty()); // Checking if Browsing History is Empty
+            addSiteToHistory("www.google.co.in"); // Navigating to Google
+            addSiteToHistory("www.facebook.com"); // Navigating to Facebook
+            addSiteToHistory("www.upgrad.com"); // Navigating to UpGrad
+            System.out.println(isBrowsingHistoryEmpty()); // Checking if Browsing History is Empty
+            System.out.println(mostRecentlyVisitedSite()); // Fetching most recently visited site (UpGrad)
+            addSiteToHistory("www.youtube.com"); // Navigating to Youtube
+            goBackInTime(2); // Going back by 2 sites
+            addSiteToHistory("www.yahoo.com"); // Navigating to UpGrad platform site
+            System.out.println(mostRecentlyVisitedSite()); // Fetching most recently visited site (UpGrad Learn Platform)
+            printBrowsingHistory(); // Printing browsing history
+
+        }
+
+    }
+
